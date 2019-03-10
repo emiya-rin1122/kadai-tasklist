97fb1f377eacdf26aac74ad59a84a1a40cd50bad
emiya.rin <haretarinaitari@gmail.com>
2019-03-10 08:51:44 +0000

    login and post rename

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
   before_action :require_user_logged_in, only: [:show, :edit, :create, :update, :destroy]
+  before_action :correct_user, only: [:show, :edit, :destroy]
   
     def index
       if logged_in?
@@ -49,13 +50,20 @@ class TasksController < ApplicationController
       redirect_to tasks_url
     end
 
-private
+  private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-def task_params
-  params.require(:task).permit(:content, :status)
-end
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 end
